9c88631171c882d96d8aece6a99506a4fe8f46d9
Gerardo <gpadil@icloud.com>
2022-03-26 19:54:53 -0600

    added log delete function

--- client/index.js
+++ client/index.js
@@ -1,7 +1,5 @@
-
 console.log('Got Connection')
 
-const gradient = document.querySelector(".gradient");
 const getAllBtn = document.querySelector('#all')
 const createForm = document.querySelector('#create-form')
 const newBikeInput = document.querySelector('#bike')
@@ -9,16 +7,10 @@ const newTrackInput = document.querySelector('#track')
 const newWeatherDropDown = document.querySelector('select')
 const newLapsInput = document.querySelector('#laps')
 const newLapTime = document.querySelector('#lap-time')
-const newLikesText = document.querySelector('textarea')
 const logsContainer = document.querySelector('section')
-const deleteBtn = document.querySelector('#deleteBtn')
 let imageURL = document.querySelector('#img')
 
 const baseURL = `http://localhost:5555`
-
-// function onMouseMove(event) {
-//   gradient.style.backgroundImage = 'radial-gradient(at ' + event.clientX + 'px ' + event.clientY + 'px, rgba(0, 2, 128, 0.9) 0, #424242 90%)';
-// }
 
 function createLogCard(log) {
   let logCard = document.createElement('div')
@@ -32,13 +24,6 @@ function createLogCard(log) {
   <button onclick="deleteLog(${log.id})">delete</button>
   </div>`
   logsContainer.appendChild(logCard)
-
-//   let deleteBtn = document.createElement('button')
-//   deleteBtn.textContent = 'DELETE'
-//   deleteBtn.innerHTML = `<div id="deleteBtn" >Delete</div>`
-//   deleteBtn.addEventListener('click', deleteLog)
-  
-//   charCard.appendChild(deleteBtn)
 }
 function clearLogs() {
   logsContainer.innerHTML = ``
@@ -85,12 +70,22 @@ function createNewLog(event) {
    newWeatherDropDown.value = 'Sunny'
    newLapsInput.value = ''
    newLapTime.value = ''
-   newLikesText.value = ''
 }
 
-function deleteLog(event) {
-  event.preventDefault();
-  event.target.parentNode.remove()
+function displayLogs(arr) {
+  logsContainer.innerHTML = ``
+  for (let i = 0; i < arr.length; i++) {
+      createLogCard(arr[i])
+  }
+}
+
+function logsCallback({data:logs}) {
+  displayLogs(logs)
+}
+
+function deleteLog(id) {
+  axios.delete(`${baseURL}/logs/${id}`)
+  .then(logsCallback)
 }
 
 
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(express.json())
 let logs = [
     {
         id: 0,
-        imageURL: 'https://media.istockphoto.com/photos/outdoor-track-and-racing-picture-id942673134?k=20&m=942673134&s=612x612&w=0&h=egK1oNwQYa_SOwjrY0KTv-iVHvX5DuljQJN1ZYC1MiM=',
+        imageURL: 'https://images.unsplash.com/photo-1523978577004-06ba57f175aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW90b3JjeWNsZSUyMHJhY2luZ3xlbnwwfHwwfHw%3D&w=1000&q=80',
         bikeName: 'Yamaha R1', 
         trackName: 'Apex Racetrack', 
         weather: 'Sunny',
@@ -16,47 +16,36 @@ let logs = [
         lapTime: 19.96,
         likes: ['Great traction', 'Well Maintained', 'Good service']
     },
+    
     {
         id: 1,
         imageURL: 'https://media.istockphoto.com/photos/outdoor-track-and-racing-picture-id942673134?k=20&m=942673134&s=612x612&w=0&h=egK1oNwQYa_SOwjrY0KTv-iVHvX5DuljQJN1ZYC1MiM=',
         bikeName: 'Yamaha R1', 
         trackName: 'Apex Racetrack', 
-        weather: 'Sunny',
-        laps: 39,
-        lapTime: 19.96,
-        likes: ['Great traction', 'Well Maintained', 'Good service']
+        weather: 'Cloudy',
+        laps: 20,
+        lapTime: 23.56,
     }
 ]
-
-// let times = [
-//     {
-//         id: 0,
-//         input1: 15.256,
-//         input2: 15.256,
-//         input3: 15.256,
-//     }
-
-// ]
 
 app.get('/logs', (req, res) => {
     console.log('hit get logs')
     res.status(200).send(logs)
 })
 
-let id = 1
+let id = 2
 
 app.post('/logs', (req, res) => {
     let newLog = {...req.body, id}
-    newLog.likes = newLog.likes.slice(0, 3)
     logs.unshift(newLog)
     res.status(200).send(logs)
     id++
 })
 
 app.delete('/logs/:id', (req, res) => {
-        let index = logs.findIndex(logs => +logs.id === +req.params.id)
-        logs.splice(index, 1)
-        res.status(200).send(logs)
+    let index = logs.findIndex(log => +logs.id === +req.params.id)
+    logs.splice(index, 1)
+    res.status(200).send(logs)
 })
 
 app.listen(5555, () => console.log('up on 5555'))
